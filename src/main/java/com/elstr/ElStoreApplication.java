b9493832ca1fbaebd606c355a7e29a3669e10708
Evgeniy Leshevich <evgeniy.le99@gmail.com>
2022-03-19 14:38:48 +0300

    Create a Deployable War File

--- src/main/java/com/elstr/ElStoreApplication.java
+++ src/main/java/com/elstr/ElStoreApplication.java
@@ -2,9 +2,16 @@ package com.elstr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ElStoreApplication {
+public class ElStoreApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ElStoreApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElStoreApplication.class, args);
